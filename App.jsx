b35349bc4d25214d889e6544ebffb62f7ce23bf5
Ankur3386 <161721920+Ankur3386@users.noreply.github.com>
2024-09-06 23:33:32 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='p-4 m-2 bg-green-600'>React Router</div>
+    </>
+  )
+}
+
+export default App
